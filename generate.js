fa70299d1896735c12792af09cadc174f2f71f50
BuildTools <unconfigured@null.spigotmc.org>
2025-12-17 20:25:05 -0500

    Created a template system for adding new characters and then added said characters. Adjusted CSS.

--- generate.js
+++ generate.js
@@ -1,37 +1,173 @@
 /***********************
- *      Variables       *
+ *      Characters      *
  ***********************/
 const characters = [
-    {
-        class:"mySlides1",
-        img1:"images/Lark.png",
-        img2:"images/LarkClothes.png",
-        link:"https://example.com/",
-        plusDivsLe:"0",
-        plusDivsRi:"0"
-    },
-    {
-        imgClass:"mySlides2",
-        img1:"images/Liv.png",
-        img2:"images/LivClothes.png",
-        img3:"images/LivCat.png",
-        link:"https://otherexample.com/",
-        plusDivsLe:"2",
-        plusDivsRi:"2"
-    };
-]
+  {
+    name: "Lark Wren",
+    height: "6'0\"",
+    images: ["Lark.png", "LarkClothed.png", "LarkClothed1.png"],
+    link: "https://sweetandsoursaws.tumblr.com/",
 
-const newChara = document.createElement('div');
-const newImage = document.createElement('img');
+  },
+  {
+    name: "Lake Wren",
+    height: "6'0\"",
+    images: ["Lake.png"],
+    link: "https://sugarcoatedmechanic.tumblr.com/",
+ 
+  },
+  {
+    name: "Liv Wren",
+    height: "6'2\"",
+    images: ["Liv.png", "LivClothed.png", "LivClothed1.png"],
+    link: "https://sweetandsoursaws.tumblr.com/",
+ 
+  },
+  {
+    name: "Duck Wren",
+    height: "",
+    images: ["Duck.png", "DuckClothed.png", "DuckCat.png"],
+    link: "https://sweetandsoursaws.tumblr.com/",
+
+  },
+  {
+    name: "Raven&nbsp;Wren",
+    images: ["Raven.png", "RavenEyes.png","RavenClothed.png", "RavenCat.png"],
+    height: "4'0\"",
+    link: "https://sweetandsoursaws.tumblr.com/",
+
+  },
+  {
+    name: "Goose Wren",
+    height: "",
+    images: ["GooseClothed.png", "GooseCat.png"],
+    link: "https://sweetandsoursaws.tumblr.com/",
+
+  },
+  {
+    name: "Charlie Walker",
+    height: "5'5\"",
+    images: ["Charlie.png", "CharlieClothed.png"],
+    link: "https://cdwalker.tumblr.com/",
+
+  },
+  {
+    name: "Willow Asis",
+    height: "5'0\"",
+    images: ["Willow.png", "WillowClothed.png"],
+    link: "https://kingdomofbellows.tumblr.com/",
+
+  },
+  {
+    name: "Dustine Asis",
+    height: "6'5\"",
+    images: ["DustineHairless.png", "DustineClothed.png"],
+    link: "https://kingdomofbellows.tumblr.com/"
+  },
+  {
+    name: "Irene Asis",
+    height: "5'10\"",
+    images: ["Irene.png", "IreneClothed.png"],
+    link: "https://kingdomofbellows.tumblr.com/"
+  },
+  {
+    name: "Lucifer",
+    height: "5'4\"",
+    images: ["Lucifer.png", "LuciferClothed.png"],
+    link: "https://burningfeathersx.tumblr.com/"
+  },
+  {
+    name: "Duncan",
+    height: "6'3\"",
+    images: ["Duncan.png", "DuncanClothed.png"],
+    link: "https://mademypeace.tumblr.com/"
+  },
+  {
+    name: "Devang",
+    height: "6'10\"",
+    images: ["Devang.png", "DevangClothed.png"],
+    link: "https://disasterghaster.tumblr.com/"
+  },
+  {
+    name: "Röka Drochàrd",
+    height: "6'0\"",
+    images: ["RokaClothed.png"],
+    link: "https://actualtirefire.tumblr.com/"
+  },
+  {
+    name: "Rene",
+    height: "5'8\"",
+    images: ["René.png", "RenéClothed.png"],
+    link: "https://letsdrinktotruelove.tumblr.com/"
+  },
+  {
+    name: "Rook",
+    height: "6'4\"",
+    images: ["Rook.png", "RookHuman.png"],
+    link: "https://averyrook.tumblr.com/"
+  },
+  {
+    name: "Sinswyn",
+    height: "",
+    images: ["SinswynClothed.png"],
+    link: ""
+  },
+  {
+    name: "Lux Imperator",
+    height: "",
+    images: ["LuxClothed.png"],
+    link: "https://luximperator.tumblr.com/"
+  },
+  {
+    name: "Doc Worth",
+    height: "6'5\"",
+    images: ["WorthClothed.png"],
+    link: "https://sodoffyabuggers.tumblr.com/"
+  },
+];
 
 /***********************
- *      Create Div     *
- ***********************/
-newChara.classList.add('drag-handle', 'js-drag-handle', 'list_item', 'is-idle', 'js-item');
-
-/***********************
- *      Create img     *
+ *  Use the Template   *
  ***********************/
 
+const container = document.querySelector("#chart");
+const template = document.querySelector("#character-template");
+let classTicker = -1;
 
-document.getElementById("chart").appendChild(newChara);
+characters.forEach(character => {
+
+    classTicker = (classTicker + 1);
+    let cTString = classTicker.toString()
+    
+    const clone = template.content.cloneNode(true);
+  
+  clone.querySelector(".label").innerHTML =
+    `${character.name}<br>Height: ${character.height}`;
+
+  clone.querySelector(".link").href = character.link;
+
+  const slides = clone.querySelector(".slides");
+  character.images.forEach(src => {
+    const img = document.createElement("img");
+    img.src = `images/${src}`;
+    img.classList.add("mySlides" + classTicker);
+    slides.appendChild(img);
+  });
+  const buttonGen = clone.querySelector(".slidebutton");
+
+
+  const slideButton1 = document.createElement("button");
+  slideButton1.onclick= function() {plusDivs(-1, cTString)};
+  slideButton1.innerHTML="&#10094;";
+  slideButton1.classList.add("slidebutton1");
+  buttonGen.appendChild(slideButton1);
+
+  const slideButton2 = document.createElement("button");
+  slideButton2.onclick = function() {plusDivs(1, cTString)};
+  slideButton2.innerHTML ="&#10095;";
+  slideButton2.classList.add("slidebutton2");
+  buttonGen.appendChild(slideButton2);
+
+  container.appendChild(clone);
+
+});
---
+++ slideshow.js
@@ -0,0 +1,35 @@
+var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var slideId = ["mySlides0", "mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8","mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18"]
+showDivs(1, 0);
+showDivs(1, 1);
+showDivs(1, 2);
+showDivs(1, 3);
+showDivs(1, 4);
+showDivs(1, 5);
+showDivs(1, 6);
+showDivs(1, 7);
+showDivs(1, 8);
+showDivs(1, 9);
+showDivs(1, 10);
+showDivs(1, 11);
+showDivs(1, 12);
+showDivs(1, 13);
+showDivs(1, 14);
+showDivs(1, 15);
+showDivs(1, 16);
+showDivs(1, 17);
+showDivs(1, 18);
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
