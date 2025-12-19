281ebbc17034072b256397a2a27f0b78f0306e24
BuildTools <unconfigured@null.spigotmc.org>
2025-12-19 01:27:04 -0500

    fixed slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,5 +1,5 @@
-var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-var slideId = ["mySlides0", "mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8","mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18"]
+var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var slideId = ["mySlides0", "mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8","mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18","mySlides19"]
 showDivs(1, 0);
 showDivs(1, 1);
 showDivs(1, 2);
@@ -19,6 +19,7 @@ showDivs(1, 15);
 showDivs(1, 16);
 showDivs(1, 17);
 showDivs(1, 18);
+showDivs(1, 19);
 function plusDivs(n, no) {
   showDivs(slideIndex[no] += n, no);
 }
